959254eff057e5a4e5822061ba2bf44a4cb9f5e4
Aidos <aidos@paygate.kz>
2023-06-06 20:51:47 +0600

    refactor ThreadState

--- src/main/java/kz/job4j/concurrent/ThreadState.java
+++ src/main/java/kz/job4j/concurrent/ThreadState.java
@@ -8,22 +8,18 @@ public class ThreadState {
                     System.out.println(Thread.currentThread().getName());
                 }
         );
-        first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getName() + ": " + first.getState());
-        }
-        System.out.println(first.getName() + ": " + first.getState());
-
         Thread second = new Thread(
                 () -> {
                     System.out.println(Thread.currentThread().getName());
                 }
         );
+        first.start();
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
+
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + ": " + first.getState());
             System.out.println(second.getName() + ": " + second.getState());
         }
-        System.out.println(second.getName() + ": " + second.getState());
 
         System.out.println(Thread.currentThread().getName() + ": " + " done");
 
