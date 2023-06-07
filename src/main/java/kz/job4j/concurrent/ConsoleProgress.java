249f2e5db9497dfdc9b9afae1357556163ebbb8f
Aidos <aidos@paygate.kz>
2023-06-07 14:05:54 +0600

    refactor ConsoleProgress Thread.interrupt()

--- src/main/java/kz/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/kz/job4j/concurrent/ConsoleProgress.java
@@ -1,21 +1,20 @@
 package kz.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
-    private int i = 0;
-    private char[] process = new char[] {'-', '\\', '|', '/'};
 
     @Override
     public void run() {
+        int i = 0;
+        char[] process = new char[] {'-', '\\', '|', '/'};
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(500);
-                System.out.printf("\r load: " + this.process[i]);
-                this.i++;
-                if (this.i == process.length) {
-                    this.i = 0;
+                System.out.printf("\r load: " + process[i]);
+                i++;
+                if (i == process.length) {
+                    i = 0;
                 }
             } catch (InterruptedException e) {
-                ///e.printStackTrace();
                 System.out.printf("\r loaded !");
                 Thread.currentThread().interrupt();
             }
