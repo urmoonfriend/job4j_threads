06376ea5f4ad5c35aaae166e67beb24461f353e1
Aidos <aidos@paygate.kz>
2023-06-09 20:59:06 +0600

    refactor DCL

--- src/test/java/kz/job4j/DCLSingleton.java
+++ src/test/java/kz/job4j/DCLSingleton.java
@@ -5,10 +5,15 @@ public final class DCLSingleton {
 
     public static DCLSingleton instOf() {
         if (inst == null) {
-            inst = new DCLSingleton();
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
         }
         return inst;
     }
+
 
     private DCLSingleton() {
     }
