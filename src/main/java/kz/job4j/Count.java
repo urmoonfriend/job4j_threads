00df043a065803f27dcba453fed7d83ccd79d477
Aidos <aidos@paygate.kz>
2023-06-11 15:10:53 +0600

    add @ThreadSafe annotation to inspect code

---
+++ src/main/java/kz/job4j/Count.java
@@ -0,0 +1,18 @@
+package kz.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}
