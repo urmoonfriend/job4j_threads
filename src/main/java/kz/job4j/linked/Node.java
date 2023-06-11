d6572fded172f01a621e2e8451c716529d29743a
Aidos <aidos@paygate.kz>
2023-06-11 11:18:48 +0600

    add immutable class Node

---
+++ src/main/java/kz/job4j/linked/Node.java
@@ -0,0 +1,19 @@
+package kz.job4j.linked;
+
+public final class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(final Node<T> next, final T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
